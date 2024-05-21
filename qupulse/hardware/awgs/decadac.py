8068154a831aff2b076a6748d3d9f1e19a0480fb
Nomos11 <82180697+Nomos11@users.noreply.github.com>
2024-05-21 19:41:56 +0200

    include some decadac-version for sweeps

---
+++ qupulse/hardware/awgs/decadac.py
@@ -0,0 +1,348 @@
+""" this module contains helper functions to work with the DecaDAC
+"""
+
+from typing import *
+from types import MappingProxyType
+import numpy as np
+import contextlib
+import itertools
+import functools
+import warnings
+import logging
+import tqdm
+
+from qupulse.program.decadac_linspace import Command, LoopLabel, Increment, Set as _Set, Wait, LoopJmp, Play, reduce_commands
+from qupulse.utils.performance import *
+
+GPADAT_ADDR = 0x006FC0
+SCRIPT_START_ADDRESS = 49408
+
+
+def volt_to_int(voltage, offset:float=-10.0, rng:float=20):
+	return np.maximum(np.minimum((2**16-1), np.floor((voltage-offset)/rng * (2**16-1))), 0)
+
+def default_ask(serial, cmd):
+	if not isinstance(cmd, bytes):
+		cmd = cmd.encode('ascii')
+	serial.write(cmd)
+	time.sleep(0.05)
+	serial.flush()
+	resp = serial.read_all()
+	return resp
+
+def inject_int(target_address:int, value:int, serial_ask:Callable):
+	resp = serial_ask('A%i;P%i;'%(target_address, value))
+	return resp
+
+
+def inject_ascii(position:int, string:str, serial_ask:Callable, fix_1:bool=True, script_offset:int=SCRIPT_START_ADDRESS):
+	""" This method overwrites 2 chars of the uploaded script
+	"""
+	assert position%2==0
+	assert len(string) == 2, "Only 2 chars (2*8bit = 16bit) are supported."
+	
+	string = string[::-1]
+	
+	target_address = np.ceil(position/2).astype(int)+script_offset
+	value = int(bin(int.from_bytes(string.encode(), 'big')), 2)
+
+	resp = inject_int(target_address, value)
+	
+	if fix_1:
+		""" 
+		The Problem: the overwritten chars are also written to the first 16 byte.
+		When running the inject call to overwrite it back to "*1" things work,.
+		But not in this if here. also not with the sleep... (but when prun timed, the call is below 1ms (but probably close to 1ms))
+		But reading requires a wait of ~60ms...
+		"""
+		
+		time.sleep(0.5)
+		inject_ascii(position=0, string="*1", fix_1=False, script_offset=script_offset)
+
+def download_16bit(index, serial_ask) -> int:
+	""" This function download 16bit from an arbitrary position within the whole memory.
+	"""
+	resp = serial_ask('A%i;p;&p;'%(index))
+	
+	s = str(resp).split("!")
+	decimal_rep = int(s[1][1:])
+
+	return decimal_rep
+
+
+def get_chars_from_script(index:int, serial_ask, script_offset:int=SCRIPT_START_ADDRESS) -> List[str]:
+	""" This function gets a char at a given position from the uploaded scan. This function reads a 16 bit block, which contains 2 8bit chars.
+	"""
+
+	decimal_rep = download_16bit(index+script_offset, serial_ask=serial_ask)
+	string = f"{bin(decimal_rep)[2:]:0>16}"
+	
+	res = []
+	for sub in [string[8:], string[:8]]:
+		try: 
+			c1 = int(f"0b{sub}", 2)
+			c1 = c1.to_bytes((c1.bit_length() + 7) // 8, 'big').decode()
+			res.append(c1)
+		except:
+			print(f"Could not decode {sub}")
+			pass
+	
+	return res
+
+def download_script(script_length:int, serial_ask, script_offset=SCRIPT_START_ADDRESS) -> str:
+	""" This function downloads the uploaded script.
+	"""
+
+	res = []
+	for i in tqdm.tqdm(range(np.ceil(script_length/2).astype(int))):
+		res.extend(get_chars_from_script(i, serial_ask=serial_ask, script_offset=script_offset))
+	return "".join(res)
+
+
+def upload_script(script:str, serial_ask) -> str:
+	""" This function uploads a given script.
+	"""
+
+	# removes the comments behind each line:
+	script = "\n".join([l.split("(")[0] for l in script.split("\n")])
+	
+	script = script.replace(' ', '')
+	script = script.replace('\n', '')
+	assert script.startswith("{")
+	assert script.endswith("};")
+
+	resp = serial_ask(script)
+
+	return resp
+
+def run_script(serial_ask, label:int=1):
+
+	resp = serial_ask(f"X{label};")
+
+	return resp
+
+def stop_script(serial_ask):
+
+	return run_script(serial_ask, label=0)
+
+def LZ77_to_linspace_commands(lz77_compressed, dt=1e+5) -> List[Command]:
+	""" This function creates a list of Linspace Commands from a lz77 compressed pulse. The LZ77 compression requires allow_intermediates=False, allow_reconstructions_using_reconstructions=False. Further using_diffs=True should be helpful.
+	"""
+
+	print_intermediate_programs = False
+	print_index_calculations = False
+
+	if isinstance(dt, (int, float)):
+		dt = np.ones(len(lz77_compressed))*dt
+	assert len(dt) == len(lz77_compressed)
+
+	loop_indecies = range(int(10_000_000_000_000)).__iter__()
+
+	commands = []
+	unrolled_step_index = []
+	for s, (o, d, v) in enumerate(lz77_compressed):
+		if print_intermediate_programs or print_index_calculations: print(); 
+		if print_intermediate_programs: print((o, d, v))
+		commands.append([])
+		unrolled_step_index.append((unrolled_step_index[-1]+1 if len(unrolled_step_index)>0 else 0))
+		if v is not None:
+			for i, a in enumerate(v):
+				if a != 0:
+					commands[-1].append(Increment(i, a, None))
+			commands[-1].append(Wait(dt[s]))
+		if d != 0:
+			lx = next(loop_indecies)
+			r = d//o
+			assert r > 0
+			if print_index_calculations: 
+				print(o, len(commands))
+				print((o, d, v))
+				print(s, (o, d), r, unrolled_step_index, unrolled_step_index[-1]-o)
+			temp = np.where(np.array(unrolled_step_index) == unrolled_step_index[-1]-o)[0]
+			assert len(temp) > 0, "Something seams to be off with the indexing. Likely due to jumping somewhere into a loop and not to its start."
+			eff = temp[-1]
+			if print_index_calculations: print(eff)
+			commands[eff].insert(0, LoopLabel(lx, r+1))
+			commands[-2].append(LoopJmp(lx))
+			foo = unrolled_step_index[-1]
+			for _o in range(len(unrolled_step_index)):
+				if len(unrolled_step_index)-_o-1-1 < eff: break
+				if print_index_calculations: print(_o, r, _o)
+				unrolled_step_index[-_o-1] += d
+				if print_index_calculations: print(unrolled_step_index)
+		if print_index_calculations: print(unrolled_step_index)
+		if print_intermediate_programs: print("\n".join([str(c) for c in commands]))
+
+	if print_intermediate_programs or print_index_calculations: print(commands)
+
+	# flattening the commands
+	commands = [c for cc in commands for c in cc]
+
+	return commands
+
+def translate_command_list_to_ascii(commands:List[Command], channel_mapping:Union[Dict[int, int], None]=None, volt_in_dac_basis:bool=False) -> str:
+
+	boards:Union[List[int], None] = None # TODO get this list from the used channel
+	
+	if channel_mapping is None:
+		channel_mapping = {i:i for i in range(5*4)}
+
+	loops = {}
+	last_used_loop_addrs = 1
+	last_used_memory_addrs = 45056
+
+	res = ["*1:"]
+
+	if boards is None:
+		boards = [0, 1, 2, 3, 4]
+	for b in boards:
+		res.append(f"B{b};M2;")
+
+	if volt_in_dac_basis:
+		conversion_fn = lambda v: v
+	else:
+		conversion_fn = volt_to_int
+	   
+
+	# setting up the main trigger
+	"""
+	SpecialConditions:
+	;08 = Trig1 is zero
+	;09 = Trig1 is not zero
+	;0A = Trig2 is zero
+	;0B = Trig2 is not zero
+	"""
+	last_used_loop_addrs += 1
+	res.append(f"*{last_used_loop_addrs}:X{0x800+last_used_loop_addrs};")
+
+	for cmd in commands:
+		if cmd.__class__.__name__ == "LoopLabel":
+			assert cmd.idx not in loops
+			last_used_loop_addrs += 1
+			last_used_memory_addrs += 1
+			loops[cmd.idx] = {
+				"count": cmd.count, 
+				"loop_addr": last_used_loop_addrs, 
+				"counter_addr": last_used_memory_addrs
+				}
+			res.extend([
+				f"A{loops[cmd.idx]['counter_addr']};", 
+				f"P{loops[cmd.idx]['count']};", 
+				f"*{loops[cmd.idx]['loop_addr']}:"
+				])
+		elif cmd.__class__.__name__ == "LoopJmp":
+			assert cmd.idx in loops
+			res.extend([
+				f"A{loops[cmd.idx]['counter_addr']};", 
+				f"+-1;", 
+				f"X{0x500+loops[cmd.idx]['loop_addr']};"
+				])
+		elif cmd.__class__.__name__ == "Increment":
+			if volt_in_dac_basis:
+				int_delta = int(conversion_fn(cmd.value))
+			else:
+				int_delta = int(conversion_fn(cmd.value)-conversion_fn(0))
+			res.extend([
+				f"A{1545+16*(channel_mapping[cmd.channel])};", 
+				f"+{int_delta};", 
+				f"A{1538+16*(channel_mapping[cmd.channel])};", 
+				f"P3;"
+				])
+		elif cmd.__class__.__name__ == "Set":
+			value_to_set = int(conversion_fn(cmd.value))
+			res.extend([
+				f"A{1545+16*(channel_mapping[cmd.channel])};", 
+				f"P{value_to_set};", 
+				f"A{1538+16*(channel_mapping[cmd.channel])};", 
+				f"P3;"
+				])
+		elif cmd.__class__.__name__ == "Wait":
+			d_in_us = int(round(float(cmd.duration)*1e-3)) # from ns to us
+			last_used_loop_addrs += 1
+			res.extend([
+				f"${d_in_us};", 
+				f"*{last_used_loop_addrs}:", 
+				f"X{0x300+last_used_loop_addrs};"
+				])
+		else:
+			raise NotImplementedError(f"Translating {cmd} is not implemented yet.")
+
+	res.append("X0;")
+	
+	whole_script = "".join(res)
+
+	assert last_used_loop_addrs < 255, "The program uses too many jump end points."
+	assert len(whole_script) < 7680, "The complete program is too long."
+
+	return whole_script
+
+def generate_linspace_commands_from_nparray_using_LZ77(array:np.ndarray, dt:float):
+	""" This function creates a list of Commands that represents the given array. This function is build for the DecaDAC.
+	"""
+
+	assert len(array.shape) == 2
+	assert array.shape[1] <= 20, "The last dimension should be the channel dimension. And our DecaDACs have only 20 channel."
+
+	if isinstance(dt, (float, int)):
+		dt = np.ones(array.shape[0])*dt
+	assert len(dt.shape) == 1
+	assert dt.shape[0] == array.shape[0]
+	dt = dt.astype(int)
+
+	# translate the values into DecaDAC values
+	array = volt_to_int(array).astype(int)
+
+	# add a time axis
+	timed_array = np.concatenate([dt[:, None], array], axis=1)
+	timed_array = timed_array.astype(int)
+	assert len(timed_array.shape) == 2
+	assert timed_array.shape == (array.shape[0], array.shape[1]+1)
+
+	# split off the first sets:
+	first_sets = timed_array[0, 1:]
+	diff_sets = np.diff(timed_array[:, :], axis=0)
+	diff_sets[:, 0] = dt[1:]
+
+	# calculate the LZ77 compression
+	comp = compress_array_LZ77(array=diff_sets, allow_intermediates=False, using_diffs=False, allow_reconstructions_using_reconstructions=False)
+
+	# splitting of the time axis
+	complz77 = [(o, d, (v[1:] if v is not None else None)) for o, d, v in comp]
+	compdt = [(v[0] if v is not None else 0) for o, d, v in comp]
+
+	# generate the list of commands
+	comm = LZ77_to_linspace_commands(complz77, dt=compdt)
+
+	# adding the first Set commands and the first wait to the top of the command list
+	to_append = [
+		_Set(channel=i, value=v, key=None)
+		for i, v in enumerate(first_sets)
+	] + [Wait(dt[0])]
+	comm = to_append + comm
+
+	# reduce unnecessary commands
+	comm = reduce_commands(comm)
+	return comm
+
+class DecaDACRepresentation:
+
+	def __init__(self, serial_ask:Callable):
+		self.serial_ask = serial_ask
+
+	def upload_command_list(self, commands:List[Command], channel_mapping:Union[Dict[int, int], None]=None, volt_in_dac_basis:bool=False):
+		ascii_script = translate_command_list_to_ascii(commands, channel_mapping=channel_mapping, volt_in_dac_basis=volt_in_dac_basis)
+		print(ascii_script)
+		resp = upload_script(script=f"{{{ascii_script}}};", serial_ask=self.serial_ask)
+
+	def upload_numpy_pulse(self, numpy_pulse:np.ndarray, dt:float, channel_mapping:Union[Dict[int, int], None]=None):
+
+		comm = generate_linspace_commands_from_nparray_using_LZ77(numpy_pulse, dt=dt)
+
+		self.upload_command_list(commands=comm, channel_mapping=channel_mapping, volt_in_dac_basis=True)
+
+
+	def arm(self):
+		run_script(serial_ask=self.serial_ask, label=1)
+
+	def reset(self):
+		stop_script(serial_ask=self.serial_ask)
---
+++ qupulse/program/decadac_linspace.py
@@ -0,0 +1,404 @@
+import abc
+import contextlib
+import dataclasses
+import numpy as np
+from dataclasses import dataclass
+from typing import Mapping, Optional, Sequence, ContextManager, Iterable, Tuple, Union, Dict, List, Iterator, Callable
+
+from qupulse import ChannelID
+from qupulse.program import Waveform, TimeType
+
+# this resolution is used to unify increments
+# the increments themselves remain floats
+DEFAULT_INCREMENT_RESOLUTION: float = 1e-9
+
+
+@dataclass(frozen=True)
+class DepKey:
+    """The key that identifies how a certain set command depends on iteration indices. The factors are rounded with a
+    given resolution to be independent on rounding errors.
+
+    These objects allow backends which support it to track multiple amplitudes at once.
+    """
+    factors: Tuple[int, ...]
+
+    @classmethod
+    def from_voltages(cls, voltages: Sequence[float], resolution: float):
+        # remove trailing zeros
+        while voltages and voltages[-1] == 0:
+            voltages = voltages[:-1]
+        return cls(tuple(int(round(voltage / resolution)) for voltage in voltages))
+
+
+@dataclass
+class LinSpaceNode:
+    """AST node for a program that supports linear spacing of set points as well as nested sequencing and repetitions"""
+
+    def dependencies(self) -> Mapping[int, set]:
+        raise NotImplementedError
+
+
+@dataclass
+class LinSpaceHold(LinSpaceNode):
+    """Hold voltages for a given time. The voltages and the time may depend on the iteration index."""
+
+    bases: Tuple[float, ...]
+    factors: Tuple[Optional[Tuple[float, ...]], ...]
+
+    duration_base: TimeType
+    duration_factors: Optional[Tuple[TimeType, ...]]
+
+    def dependencies(self) -> Mapping[int, set]:
+        return {idx: {factors}
+                for idx, factors in enumerate(self.factors)
+                if factors}
+
+
+@dataclass
+class LinSpaceArbitraryWaveform(LinSpaceNode):
+    """This is just a wrapper to pipe arbitrary waveforms through the system."""
+    waveform: Waveform
+    channels: Tuple[ChannelID, ...]
+
+
+@dataclass
+class LinSpaceRepeat(LinSpaceNode):
+    """Repeat the body count times."""
+    body: Tuple[LinSpaceNode, ...]
+    count: int
+
+    def dependencies(self):
+        dependencies = {}
+        for node in self.body:
+            for idx, deps in node.dependencies().items():
+                dependencies.setdefault(idx, set()).update(deps)
+        return dependencies
+
+
+@dataclass
+class LinSpaceIter(LinSpaceNode):
+    """Iteration in linear space are restricted to range 0 to length.
+
+    Offsets and spacing are stored in the hold node."""
+    body: Tuple[LinSpaceNode, ...]
+    length: int
+
+    def dependencies(self):
+        dependencies = {}
+        for node in self.body:
+            for idx, deps in node.dependencies().items():
+                # remove the last elemt in index because this iteration sets it -> no external dependency
+                shortened = {dep[:-1] for dep in deps}
+                if shortened != {()}:
+                    dependencies.setdefault(idx, set()).update(shortened)
+        return dependencies
+
+
+@dataclass
+class LoopLabel:
+    idx: int
+    count: int
+
+
+@dataclass
+class Increment:
+    channel: int
+    value: float
+    dependency_key: DepKey
+
+
+@dataclass
+class Set:
+    channel: int
+    value: float
+    key: DepKey = dataclasses.field(default_factory=lambda: DepKey(()))
+
+
+@dataclass
+class Wait:
+    duration: TimeType
+
+
+@dataclass
+class LoopJmp:
+    idx: int
+
+
+@dataclass
+class Play:
+    waveform: Waveform
+    channels: Tuple[ChannelID]
+
+
+Command = Union[Increment, Set, LoopLabel, LoopJmp, Wait, Play]
+
+
+@dataclass(frozen=True)
+class DepState:
+    base: float
+    iterations: Tuple[int, ...]
+
+    def required_increment_from(self, previous: 'DepState', factors: Sequence[float]) -> float:
+        assert len(self.iterations) == len(previous.iterations)
+        assert len(self.iterations) == len(factors)
+
+        increment = self.base - previous.base
+        for old, new, factor in zip(previous.iterations, self.iterations, factors):
+            # By convention there are only two possible values for each integer here: 0 or the last index
+            # The three possible increments are none, regular and jump to next line
+
+            if old == new:
+                # we are still in the same iteration of this sweep
+                pass
+
+            elif old < new:
+                assert old == 0
+                # regular iteration, although the new value will probably be > 1, the resulting increment will be
+                # applied multiple times so only one factor is needed.
+                increment += factor
+
+            else:
+                assert new == 0
+                # we need to jump back. The old value gives us the number of increments to reverse
+                increment -= factor * old
+        return increment
+
+
+@dataclass
+class _TranslationState:
+    """This is the state of a translation of a LinSpace program to a command sequence."""
+
+    label_num: int = dataclasses.field(default=0)
+    commands: List[Command] = dataclasses.field(default_factory=list)
+    iterations: List[int] = dataclasses.field(default_factory=list)
+    active_dep: Dict[int, DepKey] = dataclasses.field(default_factory=dict)
+    dep_states: Dict[int, Dict[DepKey, DepState]] = dataclasses.field(default_factory=dict)
+    plain_voltage: Dict[int, float] = dataclasses.field(default_factory=dict)
+    resolution: float = dataclasses.field(default_factory=lambda: DEFAULT_INCREMENT_RESOLUTION)
+
+    def new_loop(self, count: int):
+        label = LoopLabel(self.label_num, count)
+        jmp = LoopJmp(self.label_num)
+        self.label_num += 1
+        return label, jmp
+
+    def get_dependency_state(self, dependencies: Mapping[int, set]):
+        return {
+            self.dep_states.get(ch, {}).get(DepKey.from_voltages(dep, self.resolution), None)
+            for ch, deps in dependencies.items()
+            for dep in deps
+        }
+
+    def set_voltage(self, channel: int, value: float):
+        key = DepKey(())
+        if self.active_dep.get(channel, None) != key or self.plain_voltage.get(channel, None) != value:
+            self.commands.append(Set(channel, value, key))
+            self.active_dep[channel] = key
+            self.plain_voltage[channel] = value
+
+    def _add_repetition_node(self, node: LinSpaceRepeat):
+        pre_dep_state = self.get_dependency_state(node.dependencies())
+        label, jmp = self.new_loop(node.count)
+        initial_position = len(self.commands)
+        self.commands.append(label)
+        self.add_node(node.body)
+        post_dep_state = self.get_dependency_state(node.dependencies())
+        if pre_dep_state != post_dep_state:
+            # hackedy
+            self.commands.pop(initial_position)
+            self.commands.append(label)
+            label.count -= 1
+            self.add_node(node.body)
+        self.commands.append(jmp)
+
+    def _add_iteration_node(self, node: LinSpaceIter):
+        self.iterations.append(0)
+        self.add_node(node.body)
+
+        if node.length > 1:
+            self.iterations[-1] = node.length
+            label, jmp = self.new_loop(node.length - 1)
+            self.commands.append(label)
+            self.add_node(node.body)
+            self.commands.append(jmp)
+        self.iterations.pop()
+
+    def _set_indexed_voltage(self, channel: int, base: float, factors: Sequence[float]):
+        dep_key = DepKey.from_voltages(voltages=factors, resolution=self.resolution)
+        new_dep_state = DepState(
+            base,
+            iterations=tuple(self.iterations)
+        )
+
+        current_dep_state = self.dep_states.setdefault(channel, {}).get(dep_key, None)
+        if current_dep_state is None:
+            assert all(it == 0 for it in self.iterations)
+            self.commands.append(Set(channel, base, dep_key))
+            self.active_dep[channel] = dep_key
+
+        else:
+            inc = new_dep_state.required_increment_from(previous=current_dep_state, factors=factors)
+
+            # we insert all inc here (also inc == 0) because it signals to activate this amplitude register
+            if inc or self.active_dep.get(channel, None) != dep_key:
+                self.commands.append(Increment(channel, inc, dep_key))
+            self.active_dep[channel] = dep_key
+        self.dep_states[channel][dep_key] = new_dep_state
+
+    def _add_hold_node(self, node: LinSpaceHold):
+        if node.duration_factors:
+            raise NotImplementedError("TODO")
+
+        for ch, (base, factors) in enumerate(zip(node.bases, node.factors)):
+            if factors is None:
+                self.set_voltage(ch, base)
+                continue
+
+            else:
+                self._set_indexed_voltage(ch, base, factors)
+
+        self.commands.append(Wait(node.duration_base))
+
+    def add_node(self, node: Union[LinSpaceNode, Sequence[LinSpaceNode]]):
+        """Translate a (sequence of) linspace node(s) to commands and add it to the internal command list."""
+        if isinstance(node, Sequence):
+            for lin_node in node:
+                self.add_node(lin_node)
+
+        elif isinstance(node, LinSpaceRepeat):
+            self._add_repetition_node(node)
+
+        elif isinstance(node, LinSpaceIter):
+            self._add_iteration_node(node)
+
+        elif isinstance(node, LinSpaceHold):
+            self._add_hold_node(node)
+
+        elif isinstance(node, LinSpaceArbitraryWaveform):
+            self.commands.append(Play(node.waveform, node.channels))
+
+        else:
+            raise TypeError("The node type is not handled", type(node), node)
+
+
+def to_increment_commands(linspace_nodes: Sequence[LinSpaceNode]) -> List[Command]:
+    """translate the given linspace node tree to a minimal sequence of set and increment commands as well as loops."""
+    state = _TranslationState()
+    state.add_node(linspace_nodes)
+    return state.commands
+
+
+def reduce_consecutive_waits(commands:List[Command]) -> Tuple[bool, List[Command]]:
+
+    changed_something = False
+    new_commands = []
+    i = 0
+    while i < len(commands):
+
+        if commands[i].__class__.__name__ == "Wait":
+            consec_waits = [commands[i]]
+            # select all consecutive waits
+            j = i+1
+            while j < len(commands) and commands[j].__class__.__name__ == "Wait":
+                consec_waits.append(commands[j])
+                j += 1
+
+            # if we have only one wait. append that wait to the new command list
+            if len(consec_waits) == 1:
+                new_commands.append(commands[i])
+            else:
+                total_wait_time = np.sum([c.duration for c in consec_waits])
+                new_commands.append(Wait(total_wait_time))
+                i = j-1 # i should now be the index of the last Wait that we looked at
+                assert commands[i] == consec_waits[-1]
+                changed_something = True
+        else:
+            new_commands.append(commands[i])
+
+        i += 1
+
+    return changed_something, new_commands
+
+def reduce_looped_waits(commands:List[Command]) -> Tuple[bool, List[Command]]:
+
+    changed_something = False
+    new_commands = []
+    i = 0
+    while i < len(commands):
+
+        if commands[i].__class__.__name__ == "LoopLabel":
+            nested_commands = []
+            # select all consecutive waits
+            j = i+1
+            while j < len(commands) and commands[j].__class__.__name__ == "Wait" and ((not (commands[j].__class__.__name__ == "LoopJmp") or (int(commands[j].idx) != int(commands[i].idx)))):
+                nested_commands.append(commands[j])
+                j += 1
+
+            # check if the loop contains only one wait
+            if commands[j].__class__.__name__ == "LoopJmp" and len(nested_commands) == 1 and nested_commands[0].__class__.__name__ == "Wait":
+
+                # then create a new wait command
+                total_wait_time = nested_commands[0].duration * commands[i].count
+                new_commands.append(Wait(total_wait_time))
+                i = j # i should now point to the loop jump. The following assert does not cover it completely, but better than nothing.
+                assert commands[i].__class__.__name__ == "LoopJmp"
+                changed_something = True
+            else:
+                new_commands.append(commands[i])
+        else:
+            new_commands.append(commands[i])
+
+        i += 1
+
+    return changed_something, new_commands
+
+
+def reduce_zero_increments(commands:List[Command]) -> Tuple[bool, List[Command]]:
+
+    changed_something = False
+    new_commands = []
+    i = 0
+    while i < len(commands):
+
+        if commands[i].__class__.__name__ == "Increment":
+            if commands[i].value != 0:
+                new_commands.append(commands[i])
+            else: 
+                changed_something = True
+        else:
+            new_commands.append(commands[i])
+
+        i += 1
+
+    return changed_something, new_commands
+
+
+
+
+def reduce_commands(commands:List[Command], functions_for_reduction:Union[List[Callable], None]=None) -> List[Command]:
+    """ This function reduces commands. E.g. waits following waits will be turned into one wait. Loops over just waits, will be combined into one wait.
+    """
+
+    if functions_for_reduction is None:
+        functions_for_reduction = [
+            reduce_zero_increments,
+            reduce_consecutive_waits, 
+            reduce_looped_waits, 
+        ]
+
+    max_tries = 100_000_000
+    n_tries = 0
+    some_change = True
+    while some_change and n_tries <= max_tries:
+        # go through all the functions and reduce the program
+        change = False
+        for fn in functions_for_reduction:
+            c, commands = fn(commands)
+            change = change or c
+        n_tries += 1
+
+        some_change = change
+
+    return commands
+
+
